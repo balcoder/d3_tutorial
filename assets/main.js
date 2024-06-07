1a67512f0eae581c64436cc7e3f41f19c56fc95d
balcoder <des.barrett@gmail.com>
2024-06-07 08:44:18 +0100

    added code for async: d3.json d3.csv d3.queue

--- assets/main.js
+++ assets/main.js
@@ -37964,13 +37964,90 @@ function plotBirthsPerMonth() {
     }
 }
 
+function jsonCsvDemo() {
+    // getting data from apis using d3.json and d3.csv
+    d3.json('./countries.json', function(error,data) {
+        if(error) throw error;
+
+        d3.select("body")
+          .selectAll("h3")
+          .data(data.geonames)
+          .enter()
+          .append("h3")
+          .text(d => d.countryName);
+    })
+
+    d3.csv('./simplemaps-worldcities-basic.csv', function(row) {
+        if(+row.pop < 10000) return;
+        return {
+            cityName: row.city,
+            countryCode: row.iso2,
+            population: +row.pop
+        }        
+    }, function(error, data) {
+        if(error, data) throw error;
+
+        console.log(data);
+    })
+}
+
+function d3QueueDemo() {
+    // managing async code with d3.queue
+    d3.queue()
+    .defer(d3.json, '../../countries.json')
+    .defer(d3.csv, '../../simplemaps-worldcities-basic.csv', function(row) {
+        if(+row.pop < 10000) return;
+        return {
+            cityName: row.city,
+            countryCode: row.iso2,
+            population: +row.pop
+            }
+        })
+        // using awaitAll to return an array of returned data
+        // .awaitAll(function(error, allData) {
+        //     if(error) throw error;
+        //     // map over countries and give each one a cities array
+        //     const data = allData[0].geonames.map(country => {
+        //         country.cities = allData[1].filter(city => {
+        //             return city.countryCode === country.countryCode;                    
+        //         });
+        //         return country;
+        //     });
+        .await(function(error, countries, cities) {
+            if(error) throw error;
+            // map over countries and give each one a cities array
+            const data = countries.geonames.map(country => {
+                country.cities = cities.filter(city => {
+                    return city.countryCode === country.countryCode;                    
+                });
+                return country;
+            });            
+            const countrySelection = d3.select('body')
+              .selectAll("div")
+              .data(data)
+              .enter()
+              .append("div");
+
+            countrySelection
+              .append("h3")
+              .text(d => d.countryName);
+              
+            countrySelection
+              .append("ul")
+              .html(d => d.cities.map(city => {                
+                let percentage = city.population / d.population * 100;
+                return `<li>${city.cityName} - ${percentage.toFixed(2)}%</li>`
+              }).join(''));
+        })
+}
+
 lifeExpectancy();
 plotLiteracyGraph();
 plotBirtshHistogram();
 plotMedianAge();
 plotPieBirths();
 plotBirthsPerMonth();
-
+d3QueueDemo();
 
 
         
